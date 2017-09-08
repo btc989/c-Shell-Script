a03e42cdaf4aa1ab5ac9b7ee1792c27675927fe8
btc989 <bclar787@gmail.com>
2017-09-08 15:01:13 -0600

    Update toyshell.h

--- toyshell.h
+++ toyshell.h
@@ -4,31 +4,35 @@ using namespace std;
 class ToyShell
 {
    public:
-      ToyShell();
-      ~ToyShell();
+    ToyShell();
+    ~ToyShell();
       
-      char * tokenize( char * command);
-      char * alias( char * workCommand);
-      char * getHistoryCommand( char * workCommand);
-     void saveHistory( char * workCommand);
+    char * tokenize( char * command);
+    char * alias( char * workCommand);
+    char * getHistoryCommand( char * workCommand);
+    void saveHistory( char * workCommand);
     
-     int execute( char * workCommand);
-     void initShellName();
-     void initTerminator();
+    int execute( char * workCommand);
       
-     string errorMessage(int status);
+    string errorMessage(int status);
 
     
-     int count;
+    int count;
     
-     //these may have to be char arrays
-     string name;
-     string terminator;
+    //these may have to be char arrays
+    string name;
+    string terminator;
     
     // history[10] ??
     //aliases[10][2] ??
     void increaseCount();
-      //void setShellName();
    private:
+    void setShellName(char * newName);
+    void setShellTerminator(char * newTerminator);
+    void outputHistory();
+    int newAlias(char * workCommand);
+    void outputAlias();
+    int saveAlias(char * fileName);
+    int readAlias(char * fileName);
      
 };
