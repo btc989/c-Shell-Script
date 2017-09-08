fb59caff20bf1e19a0a2f640167ce5b0f2dfe1de
btc989 <bclar787@gmail.com>
2017-09-08 10:27:53 -0600

    Update toyshell.cpp

--- toyshell.cpp
+++ toyshell.cpp
@@ -1,34 +1,63 @@
+#include "toyshell.h"
 #include <string>
-using namespace std;
+#include <stdlib.h>
 
-class ToyShell
+//Constructors. If no radius is specified, the default is 5.
+ToyShell::ToyShell()
 {
-   public:
-      ToyShell();
-      ~ToyShell();
-      
-     const char * tokenize(const char * command);
-     const char * alias(const char * workCommand);
-     const char * getHistoryCommand(const char * workCommand);
-     void saveHistory(const char * workCommand);
-    
-     int execute(const char * workCommand);
-     void initShellName();
-     void initTerminator();
-      
-     string errorMessage(int status);
+   count = 0;
+   name = "toyshell";
+   terminator = "->";
+}
 
+void ToyShell::increaseCount()
+{
+   count++;
+}
+
+const char * ToyShell::tokenize(const char * command){
     
-     int count;
+}
+const char * ToyShell::alias(const char * workCommand){
     
-     //these may have to be char arrays
-     string name;
-     string terminator;
+}
+const char * ToyShell::getHistoryCommand(const char * workCommand){
     
-    // history[10] ??
-    //aliases[10][2] ??
-    void increaseCount();
-      //void setShellName();
-   private:
-     
-};
+}
+void ToyShell::saveHistory(const char * workCommand){
+    
+}
+int ToyShell::execute(const char * workCommand){
+     //sets shell name
+      
+      //sets terminator
+      
+      //lists current history -> default array of 10
+      
+      //! re-executes certain command in history list
+      
+      //new alias for command
+      
+      //output all aliases that have been defined
+      
+      // savenewnames store all aliases in file
+      
+      //readnewnames read all aliases from file
+      
+      //if not a shell command try and execute as UNIX Command
+    
+      //if not UNIX Command Return error
+    
+      return status;
+}
+void ToyShell::initShellName(){
+    
+}
+void ToyShell::initTerminator(){
+    
+    
+}
+
+string ToyShell::errorMessage(int status){
+    
+}
