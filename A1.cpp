643e947cfd2f140dd6af35d7bd6d69c210b75f9a
btc989 <bclar787@gmail.com>
2017-09-09 13:34:13 -0600

    Update A1.cpp

--- A1.cpp
+++ A1.cpp
@@ -6,28 +6,17 @@ using namespace std;
 int main()
 {
    ToyShell shell;
-   //get default shellname and terminator from file
-   
-    
-    
-  //maybe make this into string
    string command;
    bool alias = true;
-   int status=0;
+   int status=0;    
+
+   //Loop until command STOP or corresponding alias is entered 
+   //MIGHT NEED TO CHANGE THIS
+  do{
+    
    //Prompt
    cout<< shell.name<<"["<<shell.count<<"]"<<shell.terminator;
    getline(cin, command); 
-   
-
-    
-    
-    
-    
-    
-   //Loop until command STOP or corresponding alias is entered 
-   //MIGHT NEED TO CHANGE THIS
-  //while(!strcmp(command, "STOP")){
-      
     shell.tokenize(command);
     //check if command is alias
     //if no more aliases are found variable is set to false;
@@ -43,19 +32,17 @@ int main()
      //Save command to History
    //   shell.saveHistory(command);
      //Execute Command
-     // status = shell.execute(command);
-      
+      status = shell.execute();
+      if(status==10)
+          break;
       //if anything but 0 is returned, call function to write out error message
       /*if(!status)
           cout<<shell.errorMessage(status)<<endl;
       */
     //increase command count
   //    shell.increaseCount();
-    //Prompt at end of loop
-  // cout<< shell.name<<"["<<shell.count<<"]"<<shell.terminator;
-  // cin.getline(command, 128);
       
-  //}
+  }while(status != 10);
   
     
   
